a1e5e2c4cf0fcdcbaf762a3c6a1ee49a87ad92ca
juliuszCezar <michal.adam.cichy@gmail.com>
2020-06-07 17:33:32 +0200

    zadanie7

---
+++ zadanie7/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int number = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["Number"] = number;
+
+            return View();
+        }
+    }
+}
